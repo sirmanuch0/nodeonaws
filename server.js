3b4d06ce66f1b2ca71280bac3a40572978629d91
Immanuel Maina <imaina673@gmail.com>
2022-10-15 15:33:06 +0300

    changes made

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 })
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log("my server is running on port 3000");
