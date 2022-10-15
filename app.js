c94681204fedcc3fc006e2beb9f0d42219a5450b
Immanuel Maina <imaina673@gmail.com>
2022-10-15 15:17:07 +0300

    nodejs app on aws

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express(); 
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+const port = process.env.port || 3000;
+
+app.listen(port, () => {
+    console.log("my server is running on port 3000");
+});
+
+const express = require('express');
+
+
+ 
+
